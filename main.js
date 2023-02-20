f6429dbb8090d8b1f3036127b2997121f0934ab6
NaderNageh55 <93205805+NaderNageh55@users.noreply.github.com>
2023-02-20 22:00:46 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,112 @@
+// DropDown
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
+
+// Progress Bar (You must click 2 clicks to move)
+
+let text_1 = document.getElementById("text-1");
+let text_2 = document.getElementById("text-2");
+let text_3 = document.getElementById("text-3");
+
+text_1.addEventListener("click", (eo) => {
+  const progressBar = document.getElementsByClassName("progress-bar")[0];
+  setInterval(() => {
+    const computedStyle = getComputedStyle(progressBar);
+    const width = parseFloat(computedStyle.getPropertyValue("--width")) || 0;
+    progressBar.style.setProperty("--width", width + 0.1);
+  }, 5);
+});
+text_2.addEventListener("click", (eo) => {
+  const progressBar = document.getElementsByClassName("progress-bar")[1];
+  setInterval(() => {
+    const computedStyle = getComputedStyle(progressBar);
+    const width = parseFloat(computedStyle.getPropertyValue("--width")) || 0;
+    progressBar.style.setProperty("--width", width + 0.1);
+  }, 5);
+});
+
+text_3.addEventListener("click", (eo) => {
+  const progressBar = document.getElementsByClassName("progress-bar")[2];
+  setInterval(() => {
+    const computedStyle = getComputedStyle(progressBar);
+    const width = parseFloat(computedStyle.getPropertyValue("--width")) || 0;
+    progressBar.style.setProperty("--width", width + 0.1);
+  }, 5);
+});
+
+//email validation
+
+let email = document.getElementById("email");
+let full = document.getElementById("search-wrapper");
+var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+var btn = document.getElementById("button-submit");
+var flg = 0;
+
+email.addEventListener("input", (eo) => {
+  if (email.value.match(pattern)) {
+    full.style.border = "1px solid green";
+    btn.disabled = false;
+    btn.style.background = "black";
+    flg = 1;
+    document.getElementById("hide").style.display = "block";
+  } else {
+    full.style.border = "1px solid red";
+    btn.disabled = true;
+    btn.style.background = "#666";
+    flg = 0;
+    document.getElementById("hide").style.display = "none";
+  }
+  if (email.value == "") {
+    full.style.border = "none";
+    btn.disabled = true;
+    flg = 0;
+    document.getElementById("hide").style.display = "none";
+  }
+});
+
+btn.addEventListener("click", (eo) => {
+  if (flg) {
+    alert("Email Submitted Successfully");
+    document.getElementById("hide").style.display = "block";
+  }
+});
+
+// increasing numbers
+
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach((counter) => {
+  counter.innerText = "0";
+
+  const updateCounter = () => {
+    const target = +counter.getAttribute("data-target");
+    const c = +counter.innerText;
+
+    const increment = target / 200000;
+    console.log(increment);
+
+    if (c < target) {
+      counter.innerText = `${Math.ceil(c + increment)}`;
+      setTimeout(updateCounter, 1);
+    } else {
+      counter.innerText = target;
+    }
+  };
+
+  updateCounter();
+});
